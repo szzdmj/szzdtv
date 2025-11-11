2de10a4fc5b4c51c91c9225f7b2cc49fde47c035
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-12 06:11:09 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.6")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
     }
 }
