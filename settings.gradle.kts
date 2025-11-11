6695db2cc40b98b0431294b38712da412db896b9
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-11 17:35:07 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,25 @@
-settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        // Resolve AndroidX/Media3 and io.github.* from official repos
+        google()
+        mavenCentral()
+
+        // Only use JitPack for com.github.* if you truly need it.
+        exclusiveContent {
+            forRepository {
+                maven("https://jitpack.io")
+            }
+            filter {
+                includeGroupByRegex("com\\.github\\..*")
+            }
+        }
+    }
+}
