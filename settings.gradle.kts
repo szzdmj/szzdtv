536b44f1735a5f610275c2a834718ad02fdd1ea4
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-11 17:34:47 +0800

    Remove plugin and dependency management from settings
    Removed plugin and dependency resolution management sections.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,1 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io") // only if still needed
-    }
-}
+settings.gradle.kts
