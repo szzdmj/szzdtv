fb0c79d25a29cbb2aaeed070be5d1e5ac237d4ae
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-13 17:05:34 +0800

    Refactor XWalkPreferences to use concise methods

--- browser/src/main/java/org/xwalk/core/XWalkPreferences.java
+++ browser/src/main/java/org/xwalk/core/XWalkPreferences.java
@@ -2,26 +2,11 @@ package org.xwalk.core;
 
 /**
  * Minimal shim for Crosswalk XWalkPreferences.
- * Provides no-op setters/getters so code referencing XWalkPreferences compiles.
  */
 public class XWalkPreferences {
-    public static void setValue(String key, boolean value) {
-        // no-op shim
-    }
-
-    public static boolean hasValue(String key) {
-        return false;
-    }
-
-    public static boolean getValue(String key) {
-        return false;
-    }
-
-    public static void setIntegerValue(String key, int value) {
-        // no-op
-    }
-
-    public static int getIntegerValue(String key) {
-        return 0;
-    }
+    public static void setValue(String key, boolean value) { }
+    public static boolean hasValue(String key) { return false; }
+    public static boolean getValue(String key) { return false; }
+    public static void setIntegerValue(String key, int value) { }
+    public static int getIntegerValue(String key) { return 0; }
 }
