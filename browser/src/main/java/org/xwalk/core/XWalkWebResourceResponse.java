7a33f148090888303c64c5170375f806d9d98c2a
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-13 16:16:22 +0800

    Create XWalkWebResourceResponse.java

---
+++ browser/src/main/java/org/xwalk/core/XWalkWebResourceResponse.java
@@ -0,0 +1,30 @@
+package org.xwalk.core;
+
+import android.webkit.WebResourceResponse;
+
+import java.io.InputStream;
+
+/**
+ * Lightweight wrapper for WebResourceResponse to serve as XWalkWebResourceResponse.
+ * Adapters can construct this and return to callers expecting XWalkWebResourceResponse.
+ */
+public class XWalkWebResourceResponse {
+    private final WebResourceResponse mResponse;
+
+    public XWalkWebResourceResponse(String mimeType, String encoding, InputStream data) {
+        this.mResponse = new WebResourceResponse(mimeType, encoding, data);
+    }
+
+    public XWalkWebResourceResponse(WebResourceResponse resp) {
+        this.mResponse = resp;
+    }
+
+    public WebResourceResponse getWebResourceResponse() {
+        return mResponse;
+    }
+
+    // Convenience accessors if code expects them
+    public Object getData() {
+        return mResponse;
+    }
+}
