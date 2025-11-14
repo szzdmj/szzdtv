8b746f9390ff03a359599d371558f28b9fd58742
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-14 10:44:34 +0800

    Create BrowserFragment.java

---
+++ smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/fragments/BrowserFragment.java
@@ -0,0 +1,13 @@
+package com.liskovsoft.smartyoutubetv.fragments;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Minimal BrowserFragment stub.
+ * Replace with real implementation when available.
+ */
+public class BrowserFragment extends Fragment {
+    public BrowserFragment() {
+        super();
+    }
+}
