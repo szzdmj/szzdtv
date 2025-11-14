f12cd27e925248510b993b5c021acc9357d0a4a0
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-14 10:42:11 +0800

    Create GenericFragment.java

---
+++ smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/fragments/FragmentManager.java  smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/fragments/GenericFragment.java
@@ -0,0 +1,12 @@
+package com.liskovsoft.smartyoutubetv.fragments;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Minimal GenericFragment stub.
+ */
+public class GenericFragment extends Fragment {
+    public GenericFragment() {
+        super();
+    }
+}
