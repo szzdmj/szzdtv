248a1453555b74e1cc4be8627686259c7d49a503
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-14 16:34:57 +0800

    Update Log.java

--- common/src/main/java/com/liskovsoft/sharedutils/mylogger/Log.java
+++ common/src/main/java/com/liskovsoft/sharedutils/mylogger/Log.java
@@ -3,9 +3,15 @@ package com.liskovsoft.sharedutils.mylogger;
 /**
  * Minimal logger shim to satisfy imports used in the project.
  * Uses android.util.Log by fully qualified name to avoid name clash with this class.
+ * Added small compatibility surface (constants and Throwable overloads) required by callers.
  */
 public final class Log {
     private static final String DEFAULT_TAG = "szzdtv";
+
+    // compatibility constant(s) used by SmartPreferences
+    public static final String LOG_TYPE_SYSTEM = "system";
+    // add more constants if callers expect them:
+    public static final String LOG_TYPE_VERBOSE = "verbose";
 
     private Log() {}
 
@@ -17,6 +23,10 @@ public final class Log {
         android.util.Log.i(tag != null ? tag : DEFAULT_TAG, msg);
     }
 
+    public static void w(String tag, String msg) {
+        android.util.Log.w(tag != null ? tag : DEFAULT_TAG, msg);
+    }
+
     public static void e(String tag, String msg) {
         android.util.Log.e(tag != null ? tag : DEFAULT_TAG, msg);
     }
@@ -25,7 +35,12 @@ public final class Log {
         android.util.Log.e(tag != null ? tag : DEFAULT_TAG, msg, t);
     }
 
-    public static void w(String tag, String msg) {
-        android.util.Log.w(tag != null ? tag : DEFAULT_TAG, msg);
+    // Compatibility overloads: callers sometimes do Log.e(TAG, exception)
+    public static void e(String tag, Throwable t) {
+        android.util.Log.e(tag != null ? tag : DEFAULT_TAG, "", t);
+    }
+
+    public static void i(String tag, Throwable t) {
+        android.util.Log.i(tag != null ? tag : DEFAULT_TAG, "", t);
     }
 }
