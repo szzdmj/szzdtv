3700d2c1845dc3338d0c49b5d3b4cbcf99566204
szzdmj <169899174+szzdmj@users.noreply.github.com>
2025-11-14 10:45:44 +0800

    Create ActivityResult class as a minimal POJO

---
+++ smartyoutubetv/src/main/java/com/liskovsoft/smartyoutubetv/fragments/ActivityResult.java
@@ -0,0 +1,13 @@
+package com.liskovsoft.smartyoutubetv.fragments;
+
+/**
+ * Minimal POJO to satisfy imports.
+ * Extend with real fields if code expects them.
+ */
+public class ActivityResult {
+    public int requestCode;
+    public int resultCode;
+    public android.content.Intent data;
+
+    public ActivityResult() {}
+}
